d6c933daa819c4c26ffa516d824d53c01eeaadd2
mjb84 <155462087+mjb84@users.noreply.github.com>
2025-01-01 22:44:10 +0100

    Update install_bindcraft.sh

--- install_bindcraft.sh
+++ install_bindcraft.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-################## BindCraft installation script
-################## specify conda/mamba folder, and installation folder for git repositories, and whether to use mamba or $pkg_manager
+################## BindCraft installation script for Kaggle
+################## specify conda/mamba folder and installation folder for git repositories
 # Default value for pkg_manager
 pkg_manager='conda'
 cuda=''
@@ -44,31 +44,20 @@ echo -e "CUDA: $cuda"
 ################## initialisation
 SECONDS=0
 
-# set paths needed for installation and check for conda installation
-install_dir=$(pwd)
+# Check for conda installation
 CONDA_BASE=$(conda info --base 2>/dev/null) || { echo -e "Error: conda is not installed or cannot be initialised."; exit 1; }
 echo -e "Conda is installed at: $CONDA_BASE"
 
-### BindCraft install begin, create base environment
-echo -e "Installing BindCraft environment\n"
-$pkg_manager create --name BindCraft python=3.10 -y || { echo -e "Error: Failed to create BindCraft conda environment"; exit 1; }
-conda env list | grep -w 'BindCraft' >/dev/null 2>&1 || { echo -e "Error: Conda environment 'BindCraft' does not exist after creation."; exit 1; }
+# Install required conda packages in the current environment
+echo -e "Installing BindCraft dependencies in the current environment\n"
 
-# Load newly created BindCraft environment
-echo -e "Loading BindCraft environment\n"
-source ${CONDA_BASE}/bin/activate ${CONDA_BASE}/envs/BindCraft || { echo -e "Error: Failed to activate the BindCraft environment."; exit 1; }
-[ "$CONDA_DEFAULT_ENV" = "BindCraft" ] || { echo -e "Error: The BindCraft environment is not active."; exit 1; }
-echo -e "BindCraft environment activated at ${CONDA_BASE}/envs/BindCraft"
-
-# install required conda packages
-echo -e "Instaling conda requirements\n"
 if [ -n "$cuda" ]; then
-    CONDA_OVERRIDE_CUDA="$cuda" $pkg_manager install pip pandas matplotlib numpy"<2.0.0" biopython scipy pdbfixer seaborn libgfortran5 tqdm jupyter ffmpeg pyrosetta fsspec py3dmol chex dm-haiku flax"<0.10.0" dm-tree joblib ml-collections immutabledict optax jaxlib=*=*cuda* jax cuda-nvcc cudnn -c conda-forge -c nvidia  --channel https://conda.graylab.jhu.edu -y || { echo -e "Error: Failed to install conda packages."; exit 1; }
+    CONDA_OVERRIDE_CUDA="$cuda" $pkg_manager install pip pandas matplotlib numpy"<2.0.0" biopython scipy pdbfixer seaborn libgfortran5 tqdm jupyter ffmpeg pyrosetta fsspec py3dmol chex dm-haiku flax"<0.10.0" dm-tree joblib ml-collections immutabledict optax jaxlib=*=*cuda* jax cuda-nvcc cudnn -c conda-forge -c nvidia --channel https://conda.graylab.jhu.edu -y || { echo -e "Error: Failed to install conda packages."; exit 1; }
 else
-    $pkg_manager install pip pandas matplotlib numpy"<2.0.0" biopython scipy pdbfixer seaborn libgfortran5 tqdm jupyter ffmpeg pyrosetta fsspec py3dmol chex dm-haiku flax"<0.10.0" dm-tree joblib ml-collections immutabledict optax jaxlib jax cuda-nvcc cudnn -c conda-forge -c nvidia  --channel https://conda.graylab.jhu.edu -y || { echo -e "Error: Failed to install conda packages."; exit 1; }
+    $pkg_manager install pip pandas matplotlib numpy"<2.0.0" biopython scipy pdbfixer seaborn libgfortran5 tqdm jupyter ffmpeg pyrosetta fsspec py3dmol chex dm-haiku flax"<0.10.0" dm-tree joblib ml-collections immutabledict optax jaxlib jax cuda-nvcc cudnn -c conda-forge -c nvidia --channel https://conda.graylab.jhu.edu -y || { echo -e "Error: Failed to install conda packages."; exit 1; }
 fi
 
-# make sure all required packages were installed
+# Verify installation of required packages
 required_packages=(pip pandas libgfortran5 matplotlib numpy biopython scipy pdbfixer seaborn tqdm jupyter ffmpeg pyrosetta fsspec py3dmol chex dm-haiku dm-tree joblib ml-collections immutabledict optax jaxlib jax cuda-nvcc cudnn)
 missing_packages=()
 
@@ -86,21 +75,19 @@ if [ ${#missing_packages[@]} -ne 0 ]; then
     exit 1
 fi
 
-# install ColabDesign
+# Install ColabDesign
 echo -e "Installing ColabDesign\n"
 pip3 install git+https://github.com/sokrypton/ColabDesign.git --no-deps || { echo -e "Error: Failed to install ColabDesign"; exit 1; }
 python -c "import colabdesign" >/dev/null 2>&1 || { echo -e "Error: colabdesign module not found after installation"; exit 1; }
 
-############################################################################################################
-############################################################################################################
-################## cleanup
-echo -e "Cleaning up ${pkg_manager} temporary files to save space\n"
+# Cleanup
+echo -e "Cleaning up $pkg_manager temporary files to save space\n"
 $pkg_manager clean -a -y
 echo -e "$pkg_manager cleaned up\n"
 
 # AlphaFold2 weights
 echo -e "Downloading AlphaFold2 model weights \n"
-params_dir="${install_dir}/params" # Changed to ensure correct path
+params_dir="$(pwd)/params"
 params_file="${params_dir}/alphafold_params_2022-12-06.tar"
 
 # download AF2 weights
@@ -108,32 +95,23 @@ mkdir -p "${params_dir}" || { echo -e "Error: Failed to create weights directory
 wget -O "${params_file}" "https://storage.googleapis.com/alphafold/alphafold_params_2022-12-06.tar" || { echo -e "Error: Failed to download AlphaFold2 weights"; exit 1; }
 [ -s "${params_file}" ] || { echo -e "Error: Could not locate downloaded AlphaFold2 weights"; exit 1; }
 
-# extract AF2 weights to params_dir
+# extract AF2 weights
 tar tf "${params_file}" >/dev/null 2>&1 || { echo -e "Error: Corrupt AlphaFold2 weights download"; exit 1; }
-tar -xvf "${params_file}" -C "${params_dir}" || { echo -e "Error: Failed to extract AlphaFold2 weights"; exit 1; }
-[ -f "${install_dir}/params/params_model_5_ptm.npz" ] || { echo -e "Error: Could not locate extracted AlphaFold2 weights"; exit 1; }
+tar -xvf "${params_file}" -C "${params_dir}" || { echo -e "Error: Failed to extract AlphaFold2weights"; exit 1; }
+[ -f "${params_dir}/params_model_5_ptm.npz" ] || { echo -e "Error: Could not locate extracted AlphaFold2 weights"; exit 1; }
 rm "${params_file}" || { echo -e "Warning: Failed to remove AlphaFold2 weights archive"; }
 
 # chmod executables
 echo -e "Changing permissions for executables\n"
-chmod +x "${install_dir}/functions/dssp" || { echo -e "Error: Failed to chmod dssp"; exit 1; }
-chmod +x "${install_dir}/functions/DAlphaBall.gcc" || { echo -e "Error: Failed to chmod DAlphaBall.gcc"; exit 1; }
+chmod +x "$(pwd)/functions/dssp" || { echo -e "Error: Failed to chmod dssp"; exit 1; }
+chmod +x "$(pwd)/functions/DAlphaBall.gcc" || { echo -e "Error: Failed to chmod DAlphaBall.gcc"; exit 1; }
 
-############################################################################################################
-############################################################################################################
-################## cleanup
-echo -e "Cleaning up ${pkg_manager} temporary files to save space\n"
+# Cleanup
+echo -e "Cleaning up $pkg_manager temporary files to save space\n"
 $pkg_manager clean -a -y
 echo -e "$pkg_manager cleaned up\n"
 
-# finish
-conda deactivate
-echo -e "BindCraft environment set up\n"
-
-
-################## finish script
-t=$SECONDS 
-echo -e "Successfully finished BindCraft installation!\n"
-echo -e "Activate environment using command: \"$pkg_manager activate BindCraft\""
-echo -e "\n"
-echo -e "Installation took $(($t / 3600)) hours, $((($t / 60) % 60)) minutes and $(($t % 60)) seconds."
+# Finish
+t=$SECONDS
+echo -e "Successfully finished BindCraft installation!"
+echo -e "\nInstallation took $(($t / 3600)) hours, $((($t / 60) % 60)) minutes and $(($t % 60)) seconds."
